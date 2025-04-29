c10a1a3752d89b4709af31fbe2105e585eb9055b
Hossein1386-coder <hosseinmirzahasani3@gmail.com>
2025-04-29 15:26:25 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
 const CACHE_NAME = 'webara-cache-v2';
 const urlsToCache = [
-    '/',
-    '/pwa.html',
-    '/index.html',
-    '/css/style.css',
-    '/images/logo-new.png',
-    '/images/icon-192x192.png',
-    '/images/icon-512x512.png',
+    '',
+    'pwa.html',
+    'index.html',
+    'css/style.css',
+    'images/logo-new.png',
+    'images/icon-192x192.png',
+    'images/icon-512x512.png',
     'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.rtl.min.css',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css',
     'https://cdn.jsdelivr.net/gh/rastikerdar/vazirmatn@v33.003/Vazirmatn-font-face.css'
